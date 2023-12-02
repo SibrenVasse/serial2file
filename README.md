8d870230bab89a8f1ac32a1eb77e89adc325faa0
Sibren Vasse <github@sibrenvasse.nl>
2023-12-02 09:37:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# serial2file
+### Usage
+Program to save serial data to file and send via TCP simultaneously. 
+~~~
+serial2file -h
+Usage: serial2file [OPTIONS] --output <PATH>
+
+Options:
+  -s, --serial <PATH>          Serial port to open [default: /dev/ttyUSB0]
+  -b, --baud-rate <BAUD_RATE>  Serial port baud rate [default: 115200]
+  -o, --output <PATH>          File to save p1 messages to
+  -p, --port <PORT>            TCP port [default: 1080]
+  -h, --help                   Print help
+  -V, --version                Print version
+~~~
+
+~~~
+# socat -d -d pty,rawer,echo=0 pty,rawer,echo=0
+2023/12/02 09:35:10 socat[10387] N PTY is /dev/pts/0
+2023/12/02 09:35:10 socat[10387] N PTY is /dev/pts/3
+2023/12/02 09:35:10 socat[10387] N starting data transfer loop with FDs [5,5] and [7,7]
+
+# cargo run -- --output /tmp/output --serial /dev/pts/0
+
+# cat data/p1message > /dev/pts/3
+
+# telnet localhost 1080
+Trying ::1...
+Connection failed: Connection refused
+Trying 127.0.0.1...
+Connected to localhost.
+Escape character is '^]'.
+/ISk5\2MT382-1000
+1-3:0.2.8(50)
+0-0:1.0.0(101209113020W)
+0-0:96.1.1(4B384547303034303436333935353037)
+1-0:1.8.1(123456.789*kWh)
+[SNIP]
+1-0:41.7.0(02.222*kW)
+1-0:61.7.0(03.333*kW)
+1-0:22.7.0(04.444*kW)
+1-0:42.7.0(05.555*kW)
+1-0:62.7.0(06.666*kW)
+0-1:24.1.0(003)
+0-1:96.1.0(3232323241424344313233343536373839)
+0-1:24.2.1(101209112500W)(12785.123*m3)
+!EF2F
+
+# tail -f /tmp/test
+1-0:21.7.0(01.111*kW)
+1-0:41.7.0(02.222*kW)
+1-0:61.7.0(03.333*kW)
+1-0:22.7.0(04.444*kW)
+1-0:42.7.0(05.555*kW)
+1-0:62.7.0(06.666*kW)
+0-1:24.1.0(003)
+0-1:96.1.0(3232323241424344313233343536373839)
+0-1:24.2.1(101209112500W)(12785.123*m3)
+!EF2F
+~~~
+
+### Compilation
+~~~
+cargo build --release
+cargo run -- -o /tmp/output -s /dev/ttyUSB0
+~~~
---
+++ src/main.rs
@@ -0,0 +1,106 @@
+use clap::Parser;
+use std::path::Path;
+use std::time::Duration;
+
+use serialport::SerialPort;
+
+use tokio::fs::{File, OpenOptions};
+use tokio::io::{self, AsyncWriteExt};
+use tokio::net::TcpListener;
+use tokio::sync::broadcast;
+use tokio::sync::broadcast::{Receiver, Sender};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Serial port to open
+    #[clap(long, short, value_name = "PATH", default_value = "/dev/ttyUSB0")]
+    serial: String,
+
+    /// Serial port baud rate
+    #[clap(short, long, value_parser = clap::value_parser!(u32).range(1..), default_value_t = 115_200)]
+    baud_rate: u32,
+
+    /// File to save p1 messages to
+    #[clap(long, short, value_name = "PATH", required = true)]
+    output: String,
+
+    /// TCP port
+    #[clap(short, long, value_parser = clap::value_parser!(u16).range(1024..), default_value_t = 1080)]
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let args: Args = Args::parse();
+    let serial_path = Path::new(&args.serial);
+    let output_path = Path::new(&args.output);
+
+    if !serial_path.exists() {
+        eprintln!("Serial port \"{:?}\" does not exist.", serial_path);
+        ::std::process::exit(1);
+    }
+
+    // Setup serial port
+    let serial_port = serialport::new(&args.serial, args.baud_rate)
+        .timeout(Duration::from_millis(10))
+        .open()
+        .expect(format!("Failed to open \"{}", args.serial).as_str());
+
+    // Setup output file
+    let output_file: File;
+    if !output_path.exists() {
+        output_file = File::create(&args.output).await?;
+    } else {
+        output_file = OpenOptions::new().append(true).open(&args.output).await?;
+    }
+
+    // Setup communication channel
+    let (channel_tx, channel_rx) = broadcast::channel::<Vec<u8>>(32);
+
+    tokio::spawn(async move {
+        listen_network(args.port, channel_rx).await.expect("Network error");
+    });
+
+    io_thread(serial_port, output_file, channel_tx).await?;
+
+    Ok(())
+}
+
+async fn listen_network(port: u16, channel_rx: Receiver<Vec<u8>>) -> io::Result<()> {
+    let tcp_listener = TcpListener::bind(format!("{}:{}", "127.0.0.1", port))
+        .await
+        .expect(format!("Unable to listen on 127.0.0.1:{}", port).as_str());
+
+    loop {
+        let (mut socket, _) = tcp_listener.accept().await?;
+        let mut rx = channel_rx.resubscribe();
+
+        tokio::spawn(async move {
+            loop {
+                let message = rx.recv().await.unwrap();
+                socket.write_all(&message).await.unwrap();
+            }
+        });
+    }
+}
+
+async fn io_thread(
+    mut serial_port: Box<dyn SerialPort>,
+    mut output_file: File,
+    channel_tx: Sender<Vec<u8>>,
+) -> io::Result<()> {
+    let mut serial_buf: Vec<u8> = vec![0; 1024];
+    loop {
+        match serial_port.read(serial_buf.as_mut_slice()) {
+            Ok(t) => {
+                channel_tx.send(serial_buf[..t].to_vec()).expect("Unable to send message over IPC");
+                output_file.write_all(&serial_buf[..t]).await?;
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+            Err(e) => {
+                panic!("{:?}", e);
+            }
+        }
+    }
+}
